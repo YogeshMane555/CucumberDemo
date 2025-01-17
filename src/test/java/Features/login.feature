28f77a624c8c66450944d5a7a82d4968a51e265b
Yogesh <Yogesh@192.168.1.33>
2025-01-18 00:58:12 +0530

    This is First Commit for Cucumber Project

---
+++ src/test/java/Features/login.feature
@@ -0,0 +1,8 @@
+Feature: Login Feature
+
+Scenario: To check Login feature with Valid Username and Password
+
+Given User able to open URL
+When User Enter Valid Username and Valid Password
+Then It should shown Successfully Message
+
---
+++ src/test/java/StepDefination/StepDef_Login.java
@@ -0,0 +1,28 @@
+package StepDefination;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDef_Login {
+
+	@Given("User able to open URL")
+	public void user_able_to_open_url() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User Enter Valid Username and Valid Password")
+	public void user_enter_valid_username_and_valid_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("It should shown Successfully Message")
+	public void it_should_shown_successfully_message() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}	
+	
+	
+}
